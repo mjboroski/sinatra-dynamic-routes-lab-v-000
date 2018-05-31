be2340750f4d96698a490c38bcf6d93924a45366
Michael Boroski <mjboroski@gmail.com>
2018-05-31 21:37:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,9 +13,11 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-  @name=params[:name].reverse
-  "#{@name}"
-
+    @number=params[:number].to_i
+    @phrase=params[:phrase]
+    do @number times
+      "#{@phrase}"
+    end
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
