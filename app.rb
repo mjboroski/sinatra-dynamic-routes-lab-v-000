a3a646980da573f8a221243a651c988469fbaf51
Michael Boroski <mjboroski@gmail.com>
2018-05-31 21:33:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,27 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name'
+    @name=params[:name].reverse
+    @name
+  end
+
+  get '/square/:number'
+
+  end
+
+  get '/say/:number/:phrase'
+
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5'
+
+  end
+
+  get '/:operation/:number1/:number2'
+
+  end
+
 
 end
