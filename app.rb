47c1d59b6660fbf0a441401c623bf1a24710f1bf
Michael Boroski <mjboroski@gmail.com>
2018-05-31 21:35:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,18 +8,25 @@ class App < Sinatra::Base
   end
 
   get '/square/:number'
-
+    @square=params[:number].to_i*params[:number].to_i
+    "#{@square}"
   end
 
   get '/say/:number/:phrase'
+  @name=params[:name].reverse
+  "#{@name}"
 
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5'
+  @name=params[:name].reverse
+  "#{@name}"
 
   end
 
   get '/:operation/:number1/:number2'
+  @name=params[:name].reverse
+  "#{@name}"
 
   end
 
